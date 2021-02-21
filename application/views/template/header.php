8849fc8f701bad236da6be7da566d48506204d46
kuritaka <32270422+kuritaka@users.noreply.github.com>
2021-02-21 23:58:19 +0900

    add favicon

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -10,6 +10,9 @@
     <title><?php echo $title; ?></title>
     <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/bootstrap/css/mystyle.css')?>" rel="stylesheet">
+    <link rel="shortcut icon" href="<?php echo base_url('assets/images/favicon.ico')?>"  type="image/ico">
+    
+    
    
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
